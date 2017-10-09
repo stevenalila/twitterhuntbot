0f2c38ebb3ec25f629ef07aa63b2cc63ae21a260
Steven Alila <alilasteve@gmail.com>
2017-10-09 21:02:51 +0300

    added handlebars to render views to html

--- index.js
+++ index.js
@@ -1,21 +1,57 @@
+var express = require('express');
+var handlebars = require('express-handlebars');
+var bodyParser = require('body-parser');
 var Twitter = require('twitter');
 var config = require('./config.js');
+var app = express();
 
+app.engine('handlebars', handlebars({defaultLayout: 'main'}), handlebars({helpers: {
+    toJSON : function(object) {
+        return JSON.stringify(object);
+    }
+}}));
+app.set('view engine', 'handlebars');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended:true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+//use our config file
 var T = new Twitter(config);
+
+//store our tweets
+var tweetStore = [];
 
 // Set up your search parameters
 var params = {
-    q: '#nodejs',
-    count: 10,
-    result_type: 'recent',
+    q: 'javascript mean developer',
+    count: 50,
     lang: 'en'
+};
+
+function gotData(err, data, next) {
+    var tweets = data.statuses;
+    for (var i = 0; i < tweets.length; i++) {
+        tweetStore.push(tweets[i].text);
+        console.log(tweetStore);
+    }
 }
 
 T.get('search/tweets', params, gotData);
 
-function gotData(err, data) {
-    var tweets = data.statuses;
-    for (var i = 0; i < tweets.length; i++) {
-        console.log(tweets[i].text);
-    }
-}
+app.get('/', function(req, res){
+
+
+    res.render('home', { tweetStore });
+
+});
+
+
+
+var server = app.listen(3000, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('All systems go on', host, port);
+});
